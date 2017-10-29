3689a6122cd3ef93ff1397113cdb349bb6c27461
unknown <chaofan.tao@gmail.com>
2017-10-29 12:13:59 -0400

    The official app

---
+++ Desktop/APP/app/src/main/java/com/example/jingchao/myapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.jingchao.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.content.Intent;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    TextView txtStatus;
+    LoginButton login_button;
+    CallbackManager callbackManager;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        initializeControls();
+        loginWithFb();
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void initializeControls(){
+        callbackManager = CallbackManager.Factory.create();
+        txtStatus = (TextView)findViewById(R.id.txtStatus);
+        login_button = (LoginButton)findViewById(R.id.login_button);
+    }
+
+    private void loginWithFb() {
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                txtStatus.setText("Login Success\n"+loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+                txtStatus.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                txtStatus.setText("Login Error: "+error.getMessage());
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
